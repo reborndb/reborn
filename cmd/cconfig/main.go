b22f931a29b4359973746e72a503d25f407da154
siddontang <siddontang@gmail.com>
2015-05-29 13:44:14 +0800

    handle multi signals

--- cmd/cconfig/main.go
+++ cmd/cconfig/main.go
@@ -97,8 +97,7 @@ func main() {
 	}
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGTERM, os.Interrupt, os.Kill)
 	go func() {
 		<-c
 

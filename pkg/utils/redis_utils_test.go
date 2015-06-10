77f551e70e970b23552633a33901cdc3a49c8d0a
siddontang <siddontang@gmail.com>
2015-06-10 14:27:34 +0800

    add some tests for ping and get info

--- pkg/utils/redis_utils_test.go
+++ pkg/utils/redis_utils_test.go
@@ -3,24 +3,46 @@
 
 package utils
 
-/*
-const (
-	redisAddr = ":6379"
+import (
+	"testing"
+
+	"github.com/alicebob/miniredis"
+	. "gopkg.in/check.v1"
 )
 
-func TestSlotSize(t *testing.T) {
-	c, _ := redis.Dial("tcp", redisAddr)
-	defer c.Close()
+func TestT(t *testing.T) {
+	TestingT(t)
+}
 
-	ret, err := SlotsInfo(redisAddr, 1023, 0)
-	log.Info(len(ret))
+type testUtilsSuite struct {
+	r    *miniredis.Miniredis
+	addr string
+	auth string
+}
 
-	if err == nil {
-		t.Error("should be error")
+func (s *testUtilsSuite) SetUpSuite(c *C) {
+	var err error
+	s.r, err = miniredis.Run()
+	c.Assert(err, IsNil)
+
+	s.addr = s.r.Addr()
+	s.auth = "abc"
+	s.r.RequireAuth(s.auth)
+}
+
+func (s *testUtilsSuite) TearDownSuite(c *C) {
+	if s.r != nil {
+		s.r.Close()
+		s.r = nil
 	}
 }
 
-func TestStat(t *testing.T) {
-	log.Info(GetRedisStat(redisAddr))
+func (s *testUtilsSuite) TestPing(c *C) {
+	err := Ping(s.addr, s.auth)
+	c.Assert(err, IsNil)
 }
-*/
+
+func (s *testUtilsSuite) TestGetInfo(c C) {
+	_, err := GetRedisInfo(s.addr, "", s.auth)
+	c.Assert(err, IsNil)
+}

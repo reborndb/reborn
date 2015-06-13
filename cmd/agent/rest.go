c2464b5e451dba1e3a23263278dbafede59428e1
siddontang <siddontang@gmail.com>
2015-06-13 16:30:56 +0800

    add http json api for dashboard test

---
+++ cmd/agent/rest.go
@@ -0,0 +1,52 @@
+// Copyright 2015 Reborndb Org. All Rights Reserved.
+// Licensed under the MIT (MIT-LICENSE.txt) license.
+
+package main
+
+import (
+	"bytes"
+	"net/http"
+
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/juju/errors"
+)
+
+//call http url and get json, then decode to objptr
+func httpCall(objPtr interface{}, url string, method string, arg interface{}) error {
+	rw := &bytes.Buffer{}
+	if arg != nil {
+		buf, err := json.Marshal(arg)
+		if err != nil {
+			return errors.Trace(err)
+		}
+		rw.Write(buf)
+	}
+
+	req, err := http.NewRequest(method, url, rw)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	if resp.StatusCode/100 != 2 {
+		return errors.Errorf("error: %d, message: %s", resp.StatusCode, string(body))
+	}
+
+	if objPtr != nil {
+		return json.Unmarshal(body, objPtr)
+	}
+
+	return nil
+}

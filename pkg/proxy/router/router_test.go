4a17ff2801ee6504835b94ed55efb4a7cd1de779
魏泽涛 <weizetao@le.com>
2016-07-24 19:08:05 +0800

    modify TestAuthCmd

--- pkg/proxy/router/router_test.go
+++ pkg/proxy/router/router_test.go
@@ -230,6 +230,21 @@ func (s *testProxyRouterSuite) testDialConn(c *C, addr string, auth string) redi
 }
 
 func (s *testProxyRouterSuite) TestAuthCmd(c *C) {
+	if len(proxyAuth) > 0 {
+		cc, err := redis.Dial("tcp", proxyAddr)
+		c.Assert(err, IsNil)
+
+		_, err = cc.Do("SET", "foo", "bar")
+		c.Assert(err, NotNil)
+		c.Assert(err, ErrorMatches, "ERR NOAUTH Authentication required")
+	} else {
+		cc, err := redis.Dial("tcp", proxyAddr)
+		c.Assert(err, IsNil)
+
+		_, err = cc.Do("SET", "foo", "bar")
+		c.Assert(err, IsNil)
+	}
+
 	cc, err := redis.Dial("tcp", proxyAddr)
 	c.Assert(err, IsNil)
 
@@ -237,10 +252,16 @@ func (s *testProxyRouterSuite) TestAuthCmd(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(ok, Equals, "OK")
 
+	_, err = cc.Do("SET", "foo", "bar")
+	c.Assert(err, IsNil)
+
 	ok, err = redis.String(cc.Do("AUTH", "Wrong-auth-key"))
 	c.Assert(err, NotNil)
 	c.Assert(err, ErrorMatches, "ERR invalid auth")
 	c.Assert(ok, Equals, "")
+
+	s.s1.store.Reset()
+	s.s2.store.Reset()
 }
 
 func (s *testProxyRouterSuite) TestSingleKeyRedisCmd(c *C) {
